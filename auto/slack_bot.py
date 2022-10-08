69ea94df17d1a821224e17a81323947e23edd2fa
Saraansh Wadkar <saraanshwadkar@gmail.com>
2022-10-08 16:42:14 -0400

    implemented 2fa using team email

--- auto/slack_bot.py
+++ auto/slack_bot.py
@@ -6,28 +6,36 @@ import json
 class SlackWrapper:
     def __init__(self, api_key):
         self.client = WebClient(token=api_key)
-        self.members = {}
-        for member in self.client.users_list()["members"]:
-            try:
-                self.members[member["real_name"].lower()] = member["id"]
-            except:
-                pass
 
     def send_message(self, first_name, last_name, block):
         try:
             print("first name")
             self.client.chat_postMessage(
-                channel=f"@{self.members[first_name+' '+last_name]}", blocks=block
-            )
-            return None
-        except:
-            print("firstinit_lastname")
-            print(f"@{first_name[0]}{last_name}")
-            self.client.chat_postMessage(
-                channel=f"@{self.members[first_name[0]+last_name]}",
+                channel=self.client.users_lookupByEmail(
+                    email=f"{first_name}@team4099.com"
+                )["user"]["id"],
                 blocks=block,
             )
             return None
+        except:
+            try:
+                print("flast_name")
+                self.client.chat_postMessage(
+                    channel=self.client.users_lookupByEmail(
+                        email=f"{first_name[0]}{last_name}@team4099.com"
+                    )["user"]["id"],
+                    blocks=block,
+                )
+                return None
+            except:
+                print("flast_name1")
+                self.client.chat_postMessage(
+                    channel=self.client.users_lookupByEmail(
+                        email=f"{first_name[0]}{last_name}1@team4099.com"
+                    )["user"]["id"],
+                    blocks=block,
+                )
+                return None
 
     def send_verification_message(self, first_name, last_name, verification_number):
         verification_text_block = [
@@ -37,7 +45,6 @@ class SlackWrapper:
                     "type": "mrkdwn",
                     "text": (
                         "*hey is this you?*\n\n"
-                        "my name is FalconTrack, and i am the fastest bot alive."
                         " i recently discovered that a new person was trying to sign"
                         " into an account with permissions on another level."
                         " is this you? pls enter the 6 digit code below into FalconTrack."
